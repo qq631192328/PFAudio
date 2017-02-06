e25385cb7d8551cf09e9dfe6b6774e5e5e112f03
honghehe <631192328@qq.com>
2017-02-06 10:31:50 +0800

    初始化项目

---
+++ PFAudio/PFAudio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PFAudio
+//
+//  Created by hpf on 17/2/6.
+//  Copyright © 2017年 zmartec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
